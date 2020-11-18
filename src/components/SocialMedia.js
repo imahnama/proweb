63a3d44bcf6fff67170800e82f707e9d6259b5d0
Daniel Otieno <danielotieno.ke@gmail.com>
2020-11-18 10:35:31 +0300

    chore:remove the dash on heading

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -7,36 +7,34 @@ import YouTube from 'react-youtube-embed';
 
 export default function SocialMedia() {
   return (
-    <div className="social-section">
-      <Container className="social-section">
-      <Row className="media-heading">
-        <h1>Join Us On Social-Media</h1>
-      </Row>
-        <Row className="col-lg-12 col-md-12 col-sm-12 social-row">
-          <Col className="col-lg-4 col-md-6 col-sm-12">
+    <div className='social-section'>
+      <Container className='social-section'>
+        <Row className='media-heading'>
+          <h1>Join Us On Social Media</h1>
+        </Row>
+        <Row className='col-lg-12 col-md-12 col-sm-12 social-row'>
+          <Col className='col-lg-4 col-md-6 col-sm-12'>
             <Card
-              className="trial"
-              style={{ height: '26.7rem', width: '21.5rem' }}
-            >
-              <FacebookProvider appId="477221329905639">
-                <EmbeddedPost href="https://www.facebook.com/AzziadNasenya/posts/2876109055956482" />
+              className='trial'
+              style={{ height: '26.7rem', width: '21.5rem' }}>
+              <FacebookProvider appId='477221329905639'>
+                <EmbeddedPost href='https://www.facebook.com/AzziadNasenya/posts/2876109055956482' />
               </FacebookProvider>
             </Card>
           </Col>
 
-          <Col className="col-lg-4 col-md-6 col-sm-12">
+          <Col className='col-lg-4 col-md-6 col-sm-12'>
             <Card
-              className="trial"
-              style={{ height: '26.7rem', width: '21.5rem' }}
-            >
+              className='trial'
+              style={{ height: '26.7rem', width: '21.5rem' }}>
               <InstagramEmbed
-                className="instagram-media"
-                url="https://www.instagram.com/p/CF7IzUhAX2b/"
-                clientAccessToken="477221329905639|d62e81419a7bdd07433492b2a2a57e1f"
+                className='instagram-media'
+                url='https://www.instagram.com/p/CF7IzUhAX2b/'
+                clientAccessToken='477221329905639|d62e81419a7bdd07433492b2a2a57e1f'
                 maxWidth={376}
                 hideCaption={false}
-                containerTagName="div"
-                protocol=""
+                containerTagName='div'
+                protocol=''
                 injectScript
                 onLoading={() => {}}
                 onSuccess={() => {}}
@@ -46,43 +44,39 @@ export default function SocialMedia() {
             </Card>
           </Col>
 
-          <Col className="col-lg-4 col-md-6 col-sm-12">
+          <Col className='col-lg-4 col-md-6 col-sm-12'>
             <Card
-              className="trial insta-small"
-              style={{ height: '26.7rem', width: '21.5rem' }}
-            >
-              <FacebookProvider appId="477221329905639">
-                <EmbeddedPost href="https://www.facebook.com/ProgasKenya/photos/a.1184367798366403/1963904557079386/" />
+              className='trial insta-small'
+              style={{ height: '26.7rem', width: '21.5rem' }}>
+              <FacebookProvider appId='477221329905639'>
+                <EmbeddedPost href='https://www.facebook.com/ProgasKenya/photos/a.1184367798366403/1963904557079386/' />
               </FacebookProvider>
             </Card>
           </Col>
 
-          <Col className="col-lg-4 col-md-6 col-sm-12 mb-4">
+          <Col className='col-lg-4 col-md-6 col-sm-12 mb-4'>
             <Card
-              className="trial"
-              style={{ height: '20.2rem', width: '21.5rem' }}
-            >
+              className='trial'
+              style={{ height: '20.2rem', width: '21.5rem' }}>
               <TwitterTweetEmbed
                 tweetId={'1326588286165463041'}
-                data-chrome="noborders"
+                data-chrome='noborders'
               />
             </Card>
           </Col>
-          <Col className="col-lg-4 col-md-6 col-sm-12 mb-4">
+          <Col className='col-lg-4 col-md-6 col-sm-12 mb-4'>
             <Card
-              className="trial"
-              style={{ height: '20.2rem', width: '21.5rem' }}
-            >
-              <YouTube className="youtube-social" id="hdfMBVm8BkE" />
+              className='trial'
+              style={{ height: '20.2rem', width: '21.5rem' }}>
+              <YouTube className='youtube-social' id='hdfMBVm8BkE' />
             </Card>
           </Col>
-          <Col className="col-lg-4 col-md-6 col-sm-12 mb-4">
+          <Col className='col-lg-4 col-md-6 col-sm-12 mb-4'>
             <Card
-              className="trial"
-              style={{ height: '20.2rem', width: '21.5rem' }}
-            >
+              className='trial'
+              style={{ height: '20.2rem', width: '21.5rem' }}>
               <TwitterTweetEmbed
-                className="twitter-tweet"
+                className='twitter-tweet'
                 tweetId={'1308327176480190465'}
               />
             </Card>
